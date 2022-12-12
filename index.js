f46b0aae5767b18c43a6615e3d060c4aa3541748
Your Name <you@example.com>
2022-12-12 18:01:40 +0300

    done

--- index.js
+++ index.js
@@ -9,3 +9,41 @@ window.addEventListener('DOMContentLoaded' , (event) => {
 console.log('DOM fully loaded and parsed')
 
 });
+
+(function(){
+ var  btn = document.getElementById("btn");
+
+btn.addEventListener("click",function(){
+
+    alert('This is a click event');
+},false);
+
+
+
+(function(){
+ var  btn = document.getElementById("btn");
+
+ btn.addEventListener("mouseover", function(){
+
+    alert('this is a mouseover  effect');
+
+} ,false );
+ })();
+
+})
+
+const  button   = document.querySelector('.myButton');
+
+button.addEventListener('click', function(){
+console.log('button clicked');
+
+
+
+})
+
+
+function getAnime(){
+fetch;https://media.istockphoto.com/id/1406192538/video/anime-cartoon-scene-with-young-man-meeting-a-girl-in-schoolyard-with-sakura-leaves-floating.mp4?s=mp4-640x640-is&k=20&c=-rv7gns_rIKrO-fwlQXIjF-4GOiHPv3cwMC-Vt3lg9Y=;
+fetch;getAnime
+console.log("getAnime")
+}
